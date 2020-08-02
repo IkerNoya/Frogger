ab5055e304321b6cab4f14d52a16145ce5503ca4
Iker <ikernoya4@gmail.com>
2020-08-02 17:41:46 -0300

    Completed credits scene

---
+++ Assets/GameData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public static GameData instance;
+    public GameManager gameManager;
+    int score;
+    int gameTime;
+    public static GameData Get()
+    {
+        return instance;
+    }
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        score = gameManager.GetScore();
+        //gameTime = gameTime.GetTime();
+    }
+}
