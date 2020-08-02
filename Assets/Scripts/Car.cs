41297f80c301127ad72f1cfd3081a8a3afb6fe69
Iker <ikernoya4@gmail.com>
2020-08-02 12:26:01 -0300

    Changed car movement behavior

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,17 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    [SerializeField] float speed;
-    private void Start()
+    [SerializeField] float minSpeed;
+    [SerializeField] float maxSpeed;
+
+    float speed;
+    bool destination = false;
+    void Start()
     {
-        speed = 5;
+        speed = Random.Range(minSpeed, maxSpeed);
     }
     private void Update()
     {
-        Vector3 movement = new Vector3(speed, 0, 0);
+        Vector3 movement = transform.forward * speed;
         transform.position += movement * Time.deltaTime;
+    }
+    public float GetSpeed()
+    {
+        return speed;
+    }
+    public void SetSpeed(float value)
+    {
+        speed = value;
+    }
+    public bool GetDestination()
+    {
+        return destination;
+    }
+    public void SetDestination(bool value)
+    {
+        destination = value;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public List<GameObject> cars = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
